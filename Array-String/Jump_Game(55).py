c6adae7b9e24d674c3d9f7ccd0f3b3f08c7536b8
JDmon17 <119460992+JDmon17@users.noreply.github.com>
2026-08-31 16:56:44 -0400

    Create Jump_Game(55).py

---
+++ Array-String/Jump_Game(55).py
@@ -0,0 +1,14 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if len(nums) == 1:
+            return True
+        
+        target = len(nums) - 1
+
+        for i in range(len(nums)-2, -1, -1):
+            if i + nums[i] >= target:
+                target = i
+            if target == 0:
+                return True
+        
+        return False
