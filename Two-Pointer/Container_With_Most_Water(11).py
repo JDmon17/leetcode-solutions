dbd06d5fa9a837273d5512eea5962e8c160d5905
JDmon17 <119460992+JDmon17@users.noreply.github.com>
2026-09-08 21:31:16 -0400

    Create Container_With_Most_Water(11).py

---
+++ Two-Pointer/Container_With_Most_Water(11).py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        area = 0
+        left = 0
+        right = len(height) - 1
+
+        while (left < right):
+            currentArea = min(height[left], height[right]) * (right-left)
+            area = max(area, currentArea)
+
+            if(height[left] < height[right]):
+                left += 1
+            else:
+                right -= 1
+
+        return area
